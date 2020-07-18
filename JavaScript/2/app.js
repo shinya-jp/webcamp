a3630df8c35d7b85b59f7de1f8dbfa2d4b650be4
shinya-jp <shinya0733@gmail.com>
2020-07-18 16:28:08 +0900

    基本文法

---
+++ JavaScript/2/app.js
@@ -0,0 +1,63 @@
+// var int1 = 1;
+
+// var int2 = -10;
+
+// var float1 = 3.14;
+
+// var str1 = 'JavaScript'
+
+// alert(str1);
+
+// 四則演算
+// 足し算
+// alert(4 + 3);
+// 引き算
+// alert(4 - 3);
+// 掛け算
+// alert(4 * 3);
+// 割り算
+// alert(10 / 2);
+
+// 文字列の結合
+// alert('Hello' + 'World');
+// 変数の利用
+// var str1 = 'Hello';
+// var str2 = 'World!!';
+// alert(str1 + str2);
+
+// 条件分岐
+// var orange = 100;
+// var apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// }
+// else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// }
+// else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+// while文
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+// 	num = num * 2;
+// 	count = count + 1;
+// }
+
+// alert('２をかけ続けて' + max + 'を超えるように必要だった回数は' + count + '回です')
+
+// for文
+
+var i;
+var num = 0;
+
+for(i = 1; i < 11; i++){
+	num = num +i;
+}
+
+alert('１から１０まで足し算した結果は' + num + 'です')
