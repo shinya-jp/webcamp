bc426b1770cf9cc774cdc760cddf05b05ba4ff79
shinya-jp <shinya0733@gmail.com>
2020-07-22 22:22:34 +0900

    jquery学習

--- JavaScript/5/app.js
+++ JavaScript/5/app.js
@@ -1,6 +1,10 @@
 $(function(){
-	$('.box1').css({
-		'background-color': '#0000ff',
-		'height': '100px'
+	$('.box1').show(1000)
+	$('.box1').slideDown(function(){
+		$('.box1').css({
+			'background-color': '#0000ff',
+			'width': '200px',
+			'height': '100px'
+		}).slideUp(1000);
 	});
 });
