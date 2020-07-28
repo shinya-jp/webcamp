43a2837fce47837220dbfdf82fc6a29360b7aeb3
shinya-jp <shinya0733@gmail.com>
2020-07-28 22:00:34 +0900

    skipper　練習

---
+++ JavaScript/7/js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+	transiton : 'fade',
+	speed : 1000,
+	easing : 'easeOutQuart',
+	navType : 'block',
+	childrenElementType : 'div',
+	arrows : true,
+	autoPlay :true,
+	autoPlayDuration : 5000,
+	keyboardOnAlways : true,
+	hidePrevisous : false
+});
